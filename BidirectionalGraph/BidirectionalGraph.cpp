0f2371956e99c38b265b7ad6b404f7daa7358188
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2012-02-02 02:44:23 -0500

    fix #5 could not find Boost

--- BidirectionalGraph/BidirectionalGraph.cpp
+++ BidirectionalGraph/BidirectionalGraph.cpp
@@ -2,36 +2,39 @@
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
 
-//typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS> Graph; //can't use this, in_edges are not defined in this concept - it only stores out_edges for space efficiency
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS> Graph;
-
 int main(int,char*[])
 {
+  typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS> Graph;
+
   // Create a graph object
   Graph g(3);
 
   add_edge(0,1,g);
   add_edge(1,2,g);
 
-  // Get a list of incoming edges to vertex 1
   typedef boost::graph_traits < Graph >::in_edge_iterator in_edge_iterator;
 
-  std::pair<in_edge_iterator, in_edge_iterator> inEdges = in_edges(1, g);
-
+  // Get a list of incoming edges to vertex 1
   std::cout << "In edges: " << std::endl;
-  for(; inEdges.first != inEdges.second; ++inEdges.first)
+
+  for(std::pair<in_edge_iterator, in_edge_iterator> inEdges = in_edges(1, g);
+      inEdges.first != inEdges.second;
+      ++inEdges.first)
     {
     //std::cout << index[*inEdges.first] << " ";
     std::cout << *inEdges.first << " ";
     }
 
+  std::cout << std::endl;
+
   std::cout << std::endl << "Out edges: " << std::endl;
+
   // Get a list of outgoing edges from vertex 1
   typedef boost::graph_traits < Graph >::out_edge_iterator out_edge_iterator;
-  std::pair<out_edge_iterator, out_edge_iterator> outEdges =
-    out_edges(1, g);
 
-  for(; outEdges.first != outEdges.second; ++outEdges.first)
+  for(std::pair<out_edge_iterator, out_edge_iterator> outEdges = out_edges(1, g);
+      outEdges.first != outEdges.second;
+      ++outEdges.first)
     {
     std::cout << *outEdges.first << " ";
     }
